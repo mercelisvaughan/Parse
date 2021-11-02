be17dde9b09a0e43de961beefad86444174d7ea4
mercelisvaughan <mercelisvaughan@gmail.com>
2021-11-02 18:22:39 -0400

    Parse with sign in cached

--- app/src/main/java/com/example/parse/ParseApplication.java
+++ app/src/main/java/com/example/parse/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.parse;
 import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 public class ParseApplication extends Application {
@@ -10,9 +11,12 @@ public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("ZdPFbDFLLm2R2v4WZYDSDGp2T51e8HqGdnFMXLmD")
-                .clientKey("qOxBksbCDFpy1RlNM8KYqnV4mj1HdoY3hJmO1gCE")
+                .applicationId("gD5cV1OnYe1yberMDg8iqCIMt8fmbTUjuFlfuaVa")
+                .clientKey("abRj4kOmTumQEz5bcvOkk3znkYu0gFnf8czxdIDP")
                 .server("https://parseapi.back4app.com")
                 .build()
         );
